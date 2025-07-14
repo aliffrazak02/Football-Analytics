9cfb95a602aa7792b8c9b1401d388ed3ced0fe97
Aliff Iman <aliffmlg@gmail.com>
2025-07-14 19:15:22 +0800

    Add draw_triangle method to Tracker class for ball annotation in video frames

--- trackers/tracker.py
+++ trackers/tracker.py
@@ -4,6 +4,7 @@ import pickle as pkl
 import os
 import sys
 import cv2
+import numpy as np
 sys.path.append('../')
 from utils import get_center_of_bbox, get_bbox_width
 
@@ -127,6 +128,17 @@ class Tracker:
             )
         return frame
     
+    def draw_triangle(self, frame, bbox, color):
+        y = int(bbox[1])
+        x,_ = get_center_of_bbox(bbox)
+        
+        triangle_points = np.array([[x,y],[x-10,y-20],[x+10,y-20]])
+        
+        cv2.drawContours(frame, [triangle_points], 0, color, cv2.FILLED)
+        cv2.drawContours(frame, [triangle_points], 0, [0,0,0], 2)
+    
+        return frame
+    
     def draw_annotation(self, video_frames, tracks):
         output_video_frames = []
         for frame_num, frame in enumerate(video_frames):
@@ -143,6 +155,10 @@ class Tracker:
             # Draw referees
             for _, referee in referee_dict.items():
                 frame = self.draw_ellipse(frame, referee["bbox"], (0, 255, 255))
+                
+            # Draw ball
+            for track_id, ball in ball_dict.items():
+                frame = self.draw_triangle(frame, ball["bbox"], (0, 255, 0))
                 
             output_video_frames.append(frame)
             
